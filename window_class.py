55d802fda44add47fed492b6907be9f49af35991
Qvch <qwykeblu@gmail.com>
2022-11-16 17:05:53 +0200

    set perfect size for tables deleted treeview implementation

--- window_class.py
+++ window_class.py
@@ -51,40 +51,8 @@ class Window(tk.Tk):
 
 
         created_dataframe = create_dataframe_variation_series(res)
-        pt = Table(Frame_01, dataframe=created_dataframe.sort_values(by='Element'))
+        pt = Table(Frame_01, dataframe=created_dataframe.sort_values(by='Element'), height=400, width=320)
         pt.show()
-
-        # cols = list(created_dataframe.columns)
-        #
-        # tree1 = ttk.Treeview(Frame_01)
-        #
-        # tree1.grid(row = 0, column = 0, padx = 20, pady = 5)
-        # verscrlbar = ttk.Scrollbar(Frame_01,
-        #                            orient="vertical",
-        #                            command=tree1.yview)
-        # verscrlbar.grid(row=0, column=1, sticky=NS)
-        #
-        # tree1.configure(yscrollcommand=verscrlbar.set)
-        # tree1["columns"] = cols
-        # for i in cols:
-        #     tree1.column(i, anchor="w", width=50)
-        #     tree1.heading(i, text=i, anchor='w')
-        #
-        # for index, row in created_dataframe.iterrows():
-        #     tree1.insert("", 0, text=index+1, values=list(row))
-        #
-        # s = ttk.Style()
-        # s.configure('Treeview', rowheight=20)  # repace 40 with whatever you need
-        # b = Button(Frame_01, text="Показати графік", command=lambda: graph_empericaldistribution(res, ed))
-        # b.grid(row = 0, column = 0, padx = 80, pady = 5)
-
-
-
-
-
-
-
-
 
 
         Frame_title02 = Frame(Frame_00, bg="white", height = 10, width = 300, borderwidth = 2, relief=GROOVE)
@@ -100,129 +68,16 @@ class Window(tk.Tk):
         #     to=30,  # межі класу
         #     textvariable=self.option_var,
         #     wrap=True)
-        # label_classes = ttk.Label(
-        #     Frame_02,
-        #     text='Кількість класів для розбиття',
-        #     foreground='black',
-        #     font=("Arial", 10))
-        # label_classes.pack(side=BOTTOM, padx=1,pady=20)
         # spin_box.pack(side=BOTTOM, padx=15, pady=20)
 
-        pt = Table(Frame_02, dataframe=created_dataframe.sort_values(by='Element'))
+        pt = Table(Frame_02, dataframe=created_dataframe.sort_values(by='Element'), height=400, width=320)
         pt.show()
-        # tree2 = ttk.Treeview(Frame_02)
-        #
-        # tree2.grid(row = 0, column = 1, padx = 20, pady = 5)
-        # verscrlbar = ttk.Scrollbar(Frame_02,
-        #                            orient="vertical",
-        #                            command=tree2.yview)
-        # verscrlbar.grid(row=0, column=2, sticky=NS)
-        #
-        # tree2.configure(yscrollcommand=verscrlbar.set)
-        # tree2["columns"] = cols
-        # for i in cols:
-        #     tree2.column(i, anchor="w", width=50)
-        #     tree2.heading(i, text=i, anchor='w')
-        #
-        # for index, row in created_dataframe.iterrows():
-        #     tree2.insert("", 0, text=index + 1, values=list(row))
-        #
-        # s1 = ttk.Style()
-        # s1.configure('Treeview', rowheight=20, )
 
 
-        # b = Button(Frame_02, text="Показати графік",
-        #            command=lambda: graph_empericaldistribution(res, ed))
-        # b.grid(row=0, column=0, padx=80, pady=5)
-
-
-        #frame_left = Frame(variational_series, bg='violet')
-        # frame_left.pack(side=LEFT)
-        #
-        # frame_right = Frame(variational_series)
-        # frame_right.pack(side=RIGHT)
-        # res = file_choice(filename=get_file(self))
-        #
-        #
-        #
-        # created_dataframe = create_dataframe_variation_series(res)
-        #
-        #
-        # cols = list(created_dataframe.columns)
-        # label_variation_series = ttk.Label(
-        #     frame_left,
-        #     text='Варіаційний ряд',
-        #     foreground='blue',
-        #     font=("Arial", 10))
-        # label_variation_series.pack(side=TOP)
-        #
-        # tree1 = ttk.Treeview(frame_left)
-        #
-        # tree1.pack(side=LEFT)
-        # verscrlbar = ttk.Scrollbar(frame_left,
-        #                            orient="vertical",
-        #                            command=tree1.yview)
-        # verscrlbar.pack(side='right', fill='y')
-        #
-        # tree1.configure(yscrollcommand=verscrlbar.set)
-        # tree1["columns"] = cols
-        # for i in cols:
-        #     tree1.column(i, anchor="w", width=50)
-        #     tree1.heading(i, text=i, anchor='w')
-        #
-        # for index, row in created_dataframe.iterrows():
-        #     tree1.insert("", 0, text=index+1, values=list(row))
-        #
-        # s = ttk.Style()
-        # s.configure('Treeview', rowheight=20)  # repace 40 with whatever you need
-        #
-        #
         fr = get_frequency(res)
         division_into_classes(res, fr)
         ed=get_emperical_distribution(res)
-        #
-        # separator = ttk.Separator(variational_series, orient='vertical')
-        # separator.place(relx=0.5, rely=0, relwidth=0, relheight=1)
-        # spin_box = ttk.Spinbox(
-        #     frame_right,
-        #     from_=1,
-        #     to=30,  # межі класу
-        #     textvariable=self.option_var,
-        #     wrap=True)
-        # label_classes = ttk.Label(
-        #     frame_right,
-        #     text='Кількість класів для розбиття',
-        #     foreground='black',
-        #     font=("Arial", 10))
-        # label_classes.pack(side=BOTTOM, padx=1,pady=20)
-        # spin_box.pack(side=BOTTOM, padx=15, pady=20)
-        # label_divided_series = ttk.Label(
-        #     frame_right,
-        #     text='Розділення на класи',
-        #     foreground='blue',
-        #     font=("Arial", 10))
-        # label_divided_series.pack(side=TOP)
-        # tree2 = ttk.Treeview(frame_right)
-        #
-        # tree2.pack(side=RIGHT)
-        # verscrlbar = ttk.Scrollbar(frame_right,
-        #                            orient="vertical",
-        #                            command=tree2.yview)
-        # verscrlbar.pack(side='right', fill='y')
-        #
-        # tree2.configure(yscrollcommand=verscrlbar.set)
-        # tree2["columns"] = cols
-        # for i in cols:
-        #     tree2.column(i, anchor="w", width=50)
-        #     tree2.heading(i, text=i, anchor='w')
-        #
-        # for index, row in created_dataframe.iterrows():
-        #     tree2.insert("", 0, text=index + 1, values=list(row))
-        #
-        # s1 = ttk.Style()
-        # s1.configure('Treeview', rowheight=20, )
-        #
-        # current_value = tk.StringVar(value=0)
+
 
         statistical_characteristics = Frame(self)
         density_distribution_function_probabilitygrid = Frame(self)
